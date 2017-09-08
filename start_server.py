ce9754fa56a89c01a5515402d55122b7dc3e2b78
Yufei Qian <yfqian@umich.edu>
2017-09-07 20:48:44 -0700

    fix port

--- start_server.py
+++ start_server.py
@@ -10,6 +10,7 @@ import httplib2
 import json
 from flask import make_response
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -341,7 +342,7 @@ def isAdmin():
     return (level >= 5)
 
 if __name__ == '__main__':
-    port = 5000
+    port = int(os.environ.get('PORT', 5000))
     app.secret_key = 'super_secret_key'
     app.debug = True
     app.run('0.0.0.0', port) 
