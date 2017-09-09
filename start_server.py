ebb812bd14daae6b52414b13c22c20426409637d
Yufei Qian <yfqian@umich.edu>
2017-09-08 20:04:49 -0700

    fix bugs

--- start_server.py
+++ start_server.py
@@ -271,7 +271,7 @@ def gdisconnect():
     result = h.request(url, 'GET')[0]
     print 'result is '
     print result
-    if result['status'] != '200':
+    if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
         del login_session['username']
